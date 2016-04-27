cfd394158d6e0d689ab6e1a010a3b5adf3dbffe6
Jalo D. Capa <jalo.capa@gmail.com>
2016-04-27 19:25:59 +0200

    first buggy version

--- vcm.py
+++ vcm.py
@@ -2,13 +2,78 @@ import os
 import shutil
 import sys
 import yaml
+import argparse
+
+program_description = """
+This is a management program for svn or git version-controlled projects.
+"""
 
 GITHUB_USER = 'jdcapa'
 HOME = os.path.expanduser("~")
 VCM_PATH = os.path.join(HOME, ".VersionControl")
-PROJECT_DATA_FN = os.path.join(self.vcm_path, 'projects.yaml')
+PROJECT_DATA_FN = os.path.join(VCM_PATH, 'projects.yaml')
 GITHUB_https = "https://github.com/{}/{}copy"
 GITHUB_git = 'git@github.com:{}/{}.git'
+
+
+# parser set-up
+parser = argparse.ArgumentParser(description=program_description)
+
+# parser.add_argument('action',
+#                     metavar='vcm action',
+#                     type=str, default="innit",
+#                     help='Action you want performed [init|move]')
+
+parser.add_argument('-m', "--move",
+                    action="store_true",
+                    default='',
+                    dest="move",
+                    help=('Moves the dotFolder (.svn or .git to {} and ' +
+                          'creates a symlink in its stead.').format(VCM_PATH))
+
+parser.add_argument('-g', "--git-hub",
+                    action="store_true",
+                    default=False,
+                    dest="use_github",
+                    help='Is this on github?')
+
+parser.add_argument('-p', "--project",
+                    action='store',
+                    type=str, default="",
+                    dest="project_name",
+                    help='Custom project name [default: root folder name]')
+
+parser.add_argument('-vc', "--version-control",
+                    action='store',
+                    type=str, default="git",
+                    dest='vc_system',
+                    help='Version Control system [default: git]')
+
+parser.add_argument('-pa', "--path",
+                    action='store',
+                    type=str, default="",
+                    dest='path',
+                    help='Directory path [default: current directory]')
+
+parser.add_argument('-sa', "--server-address",
+                    action='store',
+                    type=str, default="",
+                    dest="server_address",
+                    help='Git or Svn server address')
+
+parser.add_argument('-u', "--user-name",
+                    action='store',
+                    type=str, default="",
+                    dest="user",
+                    help='User name for the git or svn server')
+
+parser.add_argument('-i', "--identifier",
+                    action='store',
+                    type=str, default="",
+                    dest="identifier",
+                    help='Internal database identifier, e.g. projectname-git')
+
+args = parser.parse_args()
 
 
 class VC_Project(object):
@@ -123,8 +188,29 @@ class VC_Project(object):
                         "path": self.path,
                         "vc_system": self.vc_system,
                         "user": self.user,
-                        "use_github": self.use_github
+                        "use_github": self.use_github,
                         "server_address": self.server_address}
             project_data[self.identifier] = data_set
             with open(PROJECT_DATA_FN) as projects_yaml:
                 projects_yaml.dump(project_data)
+
+
+if __name__ == '__main__':
+    if not args.path:
+        path = os.getcwd()
+    else:
+        path = args.path
+
+    if args.identifier:
+        project = VC_Project(identifier=args.identifier)
+    elif ('.git' in os.listdir() or '.svn' in os.listdir()):
+        project = VC_Project(path=args.path,
+                             vc_system=args.vc_system,
+                             project_name=args.project_name,
+                             user=args.user,
+                             use_github=args.use_github,
+                             server_address=args.server_address)
+    # Actions
+    project.write_project_data()
+    if args.move:
+        project.move_VC_dotFolder()
